9d9c4bd76151d69ae02a573cba807f7560edb77b
Taiwo <61516192+Teemoney28@users.noreply.github.com>
2020-03-02 21:57:44 -0500

    Add files via upload

---
+++ First.Tee.py
@@ -0,0 +1,44 @@
+Hello	World
+>>> a = "Hello world"
+>>> print(a)
+Hello world
+>>> name = "Taiwo"
+>>> print(name)
+Taiwo
+>>> name_1 = "Taiwo"
+>>> name_2 = "Goriola"
+>>> print(name_1,name_2)
+Taiwo Goriola
+>>> """This is the program
+Which print out all the information
+about the students
+"""
+'This is the program\nWhich print out all the information\nabout the students\n'
+>>> print("patric\'s Pencit')
+      
+SyntaxError: EOL while scanning string literal
+>>> print("Patric\"s Pencit")
+Patric"s Pencit
+>>> Patric"s Pencitprint("Peter say \"Hello world\"")
+SyntaxError: invalid syntax
+>>> print("Peter say \"Hello World\"")
+Peter say "Hello World"
+>>> print("Daniel say \"Hello World\"")
+Daniel say "Hello World"
+>>> name = input("Enter the name:")
+Enter the name:Taiwo
+>>> age = int(input("Enter the age"))
+Enter the age40
+>>> name
+'Taiwo'
+>>> age
+40
+>>> name = input("Enter the name:")
+    age = int(input("Enter the age:"))
+
+print("The name of the person is",name,"and the age is",age)
+
+
+
+          
+          
